8cfb2a2499adf8b780488f77c269449e5ec67838
crackgonzalez <cs.gonzalez3@gmail.com>
2024-05-20 01:17:07 -0400

    creacion migracion servicios

---
+++ database/migrations/2024_05_20_051539_create_servicios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->string('título');
+            $table->text('descripción');
+            $table->string('categoría');
+            $table->decimal('precio', 10, 2);
+            $table->string('ubicación');
+            $table->timestamp('fecha_publicación')->nullable();
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+
+            // Definir clave externa
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicios');
+    }
+};
